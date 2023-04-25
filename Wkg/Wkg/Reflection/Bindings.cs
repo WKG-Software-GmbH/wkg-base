3be808d83a7708b6e9643c2472d94f15279081eb
Frederik Höft <hoeft@wkg-software.de>
2023-04-25 13:21:48 +0200

    add BindingFlags helper

---
+++ Wkg/Wkg/Reflection/Bindings.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+
+namespace Wkg.Reflection;
+
+/// <summary>
+/// Provides utilities for working with <see cref="BindingFlags"/>.
+/// </summary>
+public static class Bindings
+{
+    /// <summary>
+    /// Gets the <see cref="BindingFlags"/> that represent all possible binding flags.
+    /// </summary>
+    /// <remarks>
+    /// This is equivalent to <see cref="BindingFlags.Public"/><c> | </c> <see cref="BindingFlags.NonPublic"/><c> | </c> <see cref="BindingFlags.Instance"/><c> | </c> <see cref="BindingFlags.Static"/>.
+    /// </remarks>
+    public static BindingFlags All => 
+        BindingFlags.Public
+        | BindingFlags.NonPublic
+        | BindingFlags.Instance
+        | BindingFlags.Static;
+
+    /// <summary>
+    /// Gets the <see cref="BindingFlags"/> that represent all possible binding flags, including case-insensitive binding.
+    /// </summary>
+    /// <remarks>
+    /// This is equivalent to <see cref="All"/><c> | </c> <see cref="BindingFlags.IgnoreCase"/>.
+    /// </remarks>
+    public static BindingFlags AllIgnoreCase => 
+        All | BindingFlags.IgnoreCase;
+}
--- Wkg/Wkg/Reflection/UnsafeReflection.cs
+++ Wkg/Wkg/Reflection/UnsafeReflection.cs
@@ -3,6 +3,12 @@ using System.Runtime.CompilerServices;
 
 namespace Wkg.Reflection;
 
+/// <summary>
+/// Provides reflective access to methods provided by the <see cref="Unsafe"/> class.
+/// </summary>
+/// <remarks>
+/// This class is not intended to be used directly from your code. It is used for runtime code generation by dependent libraries.
+/// </remarks>
 public static class UnsafeReflection
 {
     private static readonly MethodInfo _unsafeAs = typeof(Unsafe)
@@ -13,11 +19,24 @@ public static class UnsafeReflection
         .Where(m => m.Name is nameof(Unsafe.As) && m.GetGenericArguments().Length is 2)
         .Single();
 
+    /// <summary>
+    /// Gets the <see cref="MethodInfo"/> for the generic <see cref="Unsafe.As{T}(object)"/> method.
+    /// </summary>
     public static MethodInfo As<T>() => _unsafeAs.MakeGenericMethod(typeof(T));
 
+    /// <summary>
+    /// Gets the <see cref="MethodInfo"/> for the generic <see cref="Unsafe.As{T}(object)"/> method with the specified type argument.
+    /// </summary>
+    /// <param name="type">The type argument for the generic method.</param>
     public static MethodInfo As(Type type) => _unsafeAs.MakeGenericMethod(type);
 
+    /// <summary>
+    /// Gets the <see cref="MethodInfo"/> for the generic <see cref="Unsafe.As{TFrom, TTo}(ref TFrom)"/> method.
+    /// </summary>
     public static MethodInfo As<TFrom, TTo>() => _unsafeAsFromTo.MakeGenericMethod(typeof(TFrom), typeof(TTo));
 
+    /// <summary>
+    /// Gets the <see cref="MethodInfo"/> for the generic <see cref="Unsafe.As{TFrom, TTo}(ref TFrom)"/> method with the specified type arguments.
+    /// </summary>
     public static MethodInfo As(Type from, Type to) => _unsafeAsFromTo.MakeGenericMethod(from, to);
 }

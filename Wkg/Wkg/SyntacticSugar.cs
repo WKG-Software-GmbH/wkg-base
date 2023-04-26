29f4d6a22eddf080ad580e892494d9e3f9c80780
Frederik Höft <hoeft@wkg-software.de>
2023-04-26 14:57:14 +0200

    syntactic sugar

--- Wkg/Wkg/SyntacticSugar.cs
+++ Wkg/Wkg/SyntacticSugar.cs
@@ -23,6 +23,46 @@ public static class SyntacticSugar
     }
 
     /// <summary>
+    /// Executes the provided <paramref name="action"/> and returns an empty dummy value. Useful for using <see langword="void"/> methods in switch expressions.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public static object? Do<T>(Action<T> action, T arg)
+    {
+        action(arg);
+        return __;
+    }
+
+    /// <summary>
+    /// Executes the provided <paramref name="action"/> and returns an empty dummy value. Useful for using <see langword="void"/> methods in switch expressions.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public static object? Do<T1, T2>(Action<T1, T2> action, T1 arg1, T2 arg2)
+    {
+        action(arg1, arg2);
+        return __;
+    }
+
+    /// <summary>
+    /// Executes the provided <paramref name="action"/> and returns an empty dummy value. Useful for using <see langword="void"/> methods in switch expressions.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public static object? Do<T1, T2, T3>(Action<T1, T2, T3> action, T1 arg1, T2 arg2, T3 arg3)
+    {
+        action(arg1, arg2, arg3);
+        return __;
+    }
+
+    /// <summary>
+    /// Executes the provided <paramref name="action"/> and returns an empty dummy value. Useful for using <see langword="void"/> methods in switch expressions.
+    /// </summary>
+    [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+    public static object? Do<T1, T2, T3, T4>(Action<T1, T2, T3, T4> action, T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+    {
+        action(arg1, arg2, arg3, arg4);
+        return __;
+    }
+
+    /// <summary>
     /// Explicitly does nothing. Useful for using expression bodied syntax for empty methods. Also explicitly indicates that methods are *supposed* to be empty (as opposed to a missing implimentation).
     /// </summary>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]

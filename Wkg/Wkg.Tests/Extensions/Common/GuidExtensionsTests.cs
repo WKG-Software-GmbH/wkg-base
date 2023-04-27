65a2c2fb6991d62b315dc50833ba437ced9585ed
Frederik Höft <hoeft@wkg-software.de>
2023-04-27 14:17:25 +0200

    somehow lost high-performance version of BE UUID conversion?

---
+++ Wkg/Wkg.Tests/Extensions/Common/GuidExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Wkg.Extensions.Common.Tests;
+
+[TestClass]
+public class GuidExtensionsTests
+{
+    [TestMethod]
+    public void ToStringBigEndianTest()
+    {
+        const string expected = "2e2aff92-b697-40f0-9f5e-107999392b51";
+        Guid guid = new(Convert.FromHexString(expected.Replace("-", string.Empty)));
+        Assert.AreEqual(expected, guid.ToStringBigEndian());
+    }
+}

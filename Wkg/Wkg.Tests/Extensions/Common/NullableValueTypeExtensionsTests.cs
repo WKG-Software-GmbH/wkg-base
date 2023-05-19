72ff1340923ba0a5cd8aa2aa8c54a464f6fdf25e
Frederik Höft <hoeft@wkg-software.de>
2023-05-19 12:28:50 +0200

    NullableValueTypeExtensions

---
+++ Wkg/Wkg.Tests/Extensions/Common/NullableValueTypeExtensionsTests.cs
@@ -0,0 +1,71 @@
+﻿using Wkg.Extensions.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Wkg.Extensions.Common.Tests;
+
+[TestClass]
+public class NullableValueTypeExtensionsTests
+{
+    [TestMethod]
+    public void IsNullOrDefaultTest()
+    {
+        Guid? g;
+
+        g = null;
+        Assert.IsTrue(g.IsNullOrDefault());
+
+        g = Guid.Empty;
+        Assert.IsTrue(g.IsNullOrDefault());
+
+        g = default(Guid);
+        Assert.IsTrue(g.IsNullOrDefault());
+
+        g = Guid.NewGuid();
+        Assert.IsFalse(g.IsNullOrDefault());
+
+        g = new Guid("00000000-0000-0000-0000-000000000000");
+        Assert.IsTrue(g.IsNullOrDefault());
+
+        int? i;
+
+        i = null;
+        Assert.IsTrue(i.IsNullOrDefault());
+
+        i = 0;
+        Assert.IsTrue(i.IsNullOrDefault());
+
+        i = 1;
+        Assert.IsFalse(i.IsNullOrDefault());
+    }
+
+    [TestMethod]
+    public void IsNotNullOrDefaultTest()
+    {
+        Guid? g;
+
+        g = null;
+        Assert.IsFalse(g.HasDefinedValue());
+
+        g = Guid.Empty;
+        Assert.IsFalse(g.HasDefinedValue());
+
+        g = default(Guid);
+        Assert.IsFalse(g.HasDefinedValue());
+
+        g = Guid.NewGuid();
+        Assert.IsTrue(g.HasDefinedValue());
+
+        g = new Guid("00000000-0000-0000-0000-000000000000");
+        Assert.IsFalse(g.HasDefinedValue());
+
+        int? i;
+        i = null;
+        Assert.IsFalse(i.HasDefinedValue());
+
+        i = 0;
+        Assert.IsFalse(i.HasDefinedValue());
+
+        i = 1;
+        Assert.IsTrue(i.HasDefinedValue());
+    }
+}
---
+++ Wkg/Wkg/Extensions/Common/NullableValueTypeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Wkg.Extensions.Common;
+
+/// <summary>
+/// Contains extension methods for <see cref="Nullable{T}"/> value types.
+/// </summary>
+public static class NullableValueTypeExtensions
+{
+    /// <summary>
+    /// Checks if the provided <see cref="Nullable{T}"/> is <see langword="null"/> or if its <see cref="Nullable{T}.Value"/> property is set to the <see langword="default"/> value of value type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The value type.</typeparam>
+    /// <param name="t">The <see cref="Nullable{T}"/> instance to check.</param>
+    /// <returns><see langword="true"/> if the <see cref="Nullable{T}"/> is <see langword="null"/> or if its <see cref="Nullable{T}.Value"/> property is set to the <see langword="default"/> value of value type <typeparamref name="T"/>; otherwise, <see langword="false"/>.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsNullOrDefault<T>([NotNullWhen(false)] this T? t) where T : struct =>
+        t is null || t.Value.Equals(default(T));
+
+    /// <summary>
+    /// Checks that the provided <see cref="Nullable{T}"/> is not <see langword="null"/> and that its <see cref="Nullable{T}.Value"/> property is not set to the <see langword="default"/> value of value type <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T">The value type.</typeparam>
+    /// <param name="t">The <see cref="Nullable{T}"/> instance to check.</param>
+    /// <returns><see langword="true"/> if the <see cref="Nullable{T}"/> is not <see langword="null"/> and if its <see cref="Nullable{T}.Value"/> property is not set to the <see langword="default"/> value of value type <typeparamref name="T"/>; otherwise, <see langword="false"/>.</returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool HasDefinedValue<T>([NotNullWhen(true)] this T? t) where T : struct =>
+        !IsNullOrDefault(t);
+}

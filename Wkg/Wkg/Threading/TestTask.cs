66a0b8d9ca18cc4b4b3c0a025df87ed808a833c7
Frederik Höft <hoeft@wkg-software.de>
2023-10-21 21:37:29 +0200

    add non-locking task scheduler with limited concurrency

---
+++ Wkg/Wkg/Threading/TestTask.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wkg.Threading;
+
+[AsyncMethodBuilder(typeof(MyTaskMethodBuilder<>))]
+class MyTask<T>
+{
+    public Awaiter<T> GetAwaiter();
+}
+
+class Awaiter<T> : ICriticalNotifyCompletion
+{
+    public bool IsCompleted { get; }
+    public T GetResult();
+    public void OnCompleted(Action completion);
+    public void UnsafeOnCompleted(Action continuation) => throw new NotImplementedException();
+}
+
+struct MyTaskMethodBuilder<T>
+{
+    private AsyncTaskMethodBuilder<T> _inner;
+
+    public static MyTaskMethodBuilder<T> Create();
+
+    public void Start<TStateMachine>(ref TStateMachine stateMachine)
+        where TStateMachine : IAsyncStateMachine;
+
+    public void SetStateMachine(IAsyncStateMachine stateMachine);
+
+    public void SetException(Exception exception);
+
+    public void SetResult(T result);
+
+    public void AwaitOnCompleted<TAwaiter, TStateMachine>(
+        ref TAwaiter awaiter, ref TStateMachine stateMachine)
+        where TAwaiter : INotifyCompletion
+        where TStateMachine : IAsyncStateMachine;
+
+    public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(
+        ref TAwaiter awaiter, ref TStateMachine stateMachine)
+        where TAwaiter : ICriticalNotifyCompletion
+        where TStateMachine : IAsyncStateMachine;
+
+    public MyTask<T> Task { get; }
+}
+
+class Test
+{
+    public static async MyTask<int> TestTask()
+    {
+        await Task.Delay(1000);
+        return 42;
+    }
+}
